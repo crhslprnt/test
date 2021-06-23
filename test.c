c5942525cb3c25a4a5d7115f1a3ff607583d3614
crhslprnt <86153455+crhslprnt@users.noreply.github.com>
2021-06-23 06:03:17 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+/*
+ ==========================================================================
+ Name        : helloworld.c
+ Author      : Author
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ==========================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+	int x = 10;
+	// prints !!!Hello World!!!
+	puts("!!!Hello World !!!");
+    printf("\n x=%d \n", x);
+}
